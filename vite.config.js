3f253fe5b594979d5a1d2c59d06e5c10cd89b2e8
unknown <kingmejaouel@gmail.com>
2025-10-17 23:11:07 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base:process.env.VITE_BASE_PATH || "/portfolio-v1",
   resolve:{
     alias: {
       "@": path.resolve(__dirname,"./src"),
     },
   }
+  
 });
 
 
